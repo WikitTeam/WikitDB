1837f76413065a2f1119f69e02278aad12ee9f1a
Laimuslime <yshi03395@gmail.com>
2026-03-15 18:58:49 +0800

    Update wikitdb.config.js

--- wikitdb.config.js
+++ wikitdb.config.js
@@ -1,3 +1,4 @@
+// wikitdb.config.js
 const { FLIGHT_PARAMETERS } = require("next/dist/client/components/app-router-headers");
 
 module.exports = {
@@ -6,24 +7,24 @@ module.exports = {
     SITE_SINCE: '2026',
     SITE_AUTHOR: 'WikitDB Team',
 
-    SUPPOST_WIKI: [
+    SUPPORT_WIKI: [
         {
             NAME: "Wikidot",
             URL: "https://www.wikidot.com/",
             ImgURL: "/img/wikidot.png",
-            PAEAM: "wikidot"
+            PARAM: "wikidot"
         },
         {
             NAME: "深林文学部",
             URL: "https://deep-forest-club.wikidot.com/",
             ImgURL: "https://deep-forest-club.wdfiles.com/local--files/component%3Atheme/logo.png",
-            PAEAM: "dfc"
+            PARAM: "dfc"
         },
         {
             NAME: "地下黑市",
             URL: "https://ubmh.wikidot.com/",
             ImgURL: "https://ubmh.wdfiles.com/local--files/admin%3Atheme/ubmh.webp",
-            PAEAM: "ubmh"
+            PARAM: "ubmh"
         }
     ]
 };
